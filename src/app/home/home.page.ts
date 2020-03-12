8e94c0bbb8f61d239ff460965fdd4a05eb97ea01
SMSCodes07 <48341791+SMSCodes07@users.noreply.github.com>
2020-03-12 11:58:08 -0400

    Home Commit
    En este commit se creo el homepage que redirecciona a los usuarios a sus respectivas paginas.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router) {}
+  // Funcion para navegar al login
+  goToLogin() {
+    // Navegando al login
+    this.router.navigate(['/login']);
+    // Navegando al login
+  }
+  // Funcion para navegar al login
+  // Funcion para navegar al registro
+  goToRegister() {
+    // Navegando al registro
+    this.router.navigate(['/register']);
+    // Navegando al registro
+  }
+  // Funcion para navegar al registro
 
 }
